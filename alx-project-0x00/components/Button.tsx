2220c248958239f8e88cfc0484899341fdf3a357
Frida Mwende <mwendehfrida@gmail.com>
2025-06-01 20:19:34 +0300

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { ButtonProps } from '../interfaces';
+import { ButtonProps } from '@/interfaces';
 
-const Button: React.FC<ButtonProps> = ({ title, className }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
-    <button className={`px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-600 ${className}`}>
+    <button className={styles}>
       {title}
     </button>
   );
 };
 
 export default Button;
-
